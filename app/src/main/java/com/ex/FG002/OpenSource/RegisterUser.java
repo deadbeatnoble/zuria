b89d1b01d77b00f46620b6450a74cd96015ad719
fannuel <fannuelgulelat74@gmail.com>
2024-01-07 21:55:04 +0300

    progress load for the registry button and set current location button for store registry!

--- app/src/main/java/com/ex/FG002/OpenSource/RegisterUser.java
+++ app/src/main/java/com/ex/FG002/OpenSource/RegisterUser.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,6 +34,7 @@ public class RegisterUser extends AppCompatActivity {
     private TextInputLayout til_confirmPassword;
     private Button btn_register;
     private TextView tv_loginUser;
+    private ProgressBar pb_progressBar;
 
     String mobileNumber, password;
     FirebaseDatabase db;
@@ -50,6 +52,8 @@ public class RegisterUser extends AppCompatActivity {
         tv_loginUser = findViewById(R.id.tv_loginUser);
         btn_register = findViewById(R.id.btn_register);
 
+        pb_progressBar = findViewById(R.id.pb_progressBar);
+
         tv_loginUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,10 +65,11 @@ public class RegisterUser extends AppCompatActivity {
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                pb_progressBar.setVisibility(View.VISIBLE);
+                btn_register.setVisibility(View.GONE);
+
                 mobileNumber = getIntent().getStringExtra("usermobile");
-
                 //9.006955417367214, 38.84637434342064
-
                 String storeName = getIntent().getStringExtra("storeName");
                 String storeLocation = getIntent().getStringExtra("storeLocation");
                 String storeImage = getIntent().getStringExtra("storeImage");
@@ -76,63 +81,72 @@ public class RegisterUser extends AppCompatActivity {
 
                 password = til_confirmPassword.getEditText().getText().toString();
 
-                if (!til_newPassword.getEditText().getText().toString().equals(til_confirmPassword.getEditText().getText().toString())) {
-                    til_confirmPassword.setError("Password don't match");
+                if (til_newPassword.getEditText().getText().toString().isEmpty() || til_confirmPassword.getEditText().getText().toString().isEmpty()) {
+                    pb_progressBar.setVisibility(View.GONE);
+                    btn_register.setVisibility(View.VISIBLE);
+
+                    Toast.makeText(RegisterUser.this, "Please enter new passowrd", Toast.LENGTH_SHORT).show();
                 } else {
+                    if (!til_newPassword.getEditText().getText().toString().equals(til_confirmPassword.getEditText().getText().toString())) {
+                        pb_progressBar.setVisibility(View.GONE);
+                        btn_register.setVisibility(View.VISIBLE);
+
+                        til_confirmPassword.setError("Password don't match");
+
+                    } else {
                     /*UserDBHelper userDBHelper = new UserDBHelper(RegisterUser.this);
                     Users users = new Users(mobileNumber, password, storeImage, storeName,storeLatitude, storeLongitude, false, false);
                     userDBHelper.addUserData(users);*/
+                        storageReference = FirebaseStorage.getInstance().getReference("stores");
+                        StorageReference fileReference = storageReference.child(mobileNumber + "T" + storeName + "." + getFileExtension(Uri.parse(storeImage)));
+                        fileReference.putFile(Uri.parse(storeImage))
+                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
 
+                                                db = FirebaseDatabase.getInstance();
+                                                reference = db.getReference("Users");
 
+                                                UserDBHelper userDBHelper = new UserDBHelper(RegisterUser.this);
+                                                Users users = new Users(mobileNumber, password, uri.toString(), storeName, storeLatitude, storeLongitude, false, false);
+                                                userDBHelper.addUserData(users);
 
+                                                reference.child(mobileNumber).setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        Toast.makeText(RegisterUser.this, "Successful Registration", Toast.LENGTH_SHORT).show();
+                                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                                        userDBHelper.addUserData(users);
+                                                        startActivity(intent);
+                                                    }
+                                                });
 
-                    storageReference = FirebaseStorage.getInstance().getReference("stores");
-                    StorageReference fileReference = storageReference.child(mobileNumber + "T" + storeName + "." + getFileExtension(Uri.parse(storeImage)));
-                    fileReference.putFile(Uri.parse(storeImage))
-                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                    fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                        @Override
-                                        public void onSuccess(Uri uri) {
-
-                                            db = FirebaseDatabase.getInstance();
-                                            reference = db.getReference("Users");
-
-                                            UserDBHelper userDBHelper = new UserDBHelper(RegisterUser.this);
-                                            Users users = new Users(mobileNumber, password, uri.toString(), storeName, storeLatitude, storeLongitude, false, false);
-                                            userDBHelper.addUserData(users);
-
-                                            reference.child(mobileNumber).setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    Toast.makeText(RegisterUser.this, "Successful Registration", Toast.LENGTH_SHORT).show();
-                                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                                    userDBHelper.addUserData(users);
-                                                    startActivity(intent);
-                                                }
-                                            });
-
-                                            //Toast.makeText(RegisterUser.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(RegisterUser.this, "Failed to Register", Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                                                //Toast.makeText(RegisterUser.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        pb_progressBar.setVisibility(View.GONE);
+                                        btn_register.setVisibility(View.VISIBLE);
+                                        Toast.makeText(RegisterUser.this, "Failed to Register", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                                        pb_progressBar.setVisibility(View.VISIBLE);
+                                        btn_register.setVisibility(View.GONE);
                             /*double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
                             progress.setProgress((int)progress);*/
-                                }
-                            });
-
-
+                                    }
+                                });
+                    }
                 }
             }
         });
--- app/src/main/java/com/ex/FG002/VendorBased/VendorStoreRegistry.java
+++ app/src/main/java/com/ex/FG002/VendorBased/VendorStoreRegistry.java
@@ -3,21 +3,29 @@ package com.ex.FG002.VendorBased;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ex.FG002.OpenSource.sendOTP;
 import com.ex.FG002.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
@@ -28,13 +36,17 @@ public class VendorStoreRegistry extends AppCompatActivity {
     private FloatingActionButton fab_storePicFromGalery;
     private EditText et_storeName;
     private EditText et_storeLocation;
+    private TextView tv_setCurrentLocation;
     private Button btn_storeConfirmed;
 
     Uri imageUri;
 
+    FusedLocationProviderClient fusedLocationProviderClient;
+
 
     public static final int CAMERA_REQUEST = 100;
     public static final int STORAGE_REQUEST = 101;
+    private static final int FINE_PERMISSION_CODE = 1;
     String[]cameraPermission = {android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
     String[]storagePersmission = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
@@ -47,7 +59,10 @@ public class VendorStoreRegistry extends AppCompatActivity {
         fab_storePicFromGalery = findViewById(R.id.fab_storePicFromGallery);
         et_storeName = findViewById(R.id.et_storeName);
         et_storeLocation = findViewById(R.id.et_storeLocation);
+        tv_setCurrentLocation = findViewById(R.id.tv_setCurrentLocation);
         btn_storeConfirmed = findViewById(R.id.btn_storeConfirmed);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         fab_storePicFromGalery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +84,13 @@ public class VendorStoreRegistry extends AppCompatActivity {
             }
         });
 
+        tv_setCurrentLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLastLocation();
+            }
+        });
+
         btn_storeConfirmed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,6 +101,41 @@ public class VendorStoreRegistry extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void getLastLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                try {
+                                    et_storeLocation.setText(location.getLatitude() + ", " + location.getLongitude());
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                Toast.makeText(VendorStoreRegistry.this, "Please, turn on Location", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            Toast.makeText(VendorStoreRegistry.this, "Failed to retrieve location try manually", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        }
+                    });
+        } else {
+            requestLocationPermission();
+        }
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 100);
     }
 
     private boolean checkCameraPersmission() {
@@ -106,30 +163,43 @@ public class VendorStoreRegistry extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case CAMERA_REQUEST:
-                if (grantResults.length > 0) {
+                if (grantResults.length >= 2) { // Check if grantResults array has at least two elements
                     boolean camera_accept = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     boolean storage_accept = grantResults[1] == PackageManager.PERMISSION_GRANTED;
 
                     if (camera_accept && storage_accept) {
                         pickFromGallery();
                     } else {
-                        Toast.makeText(this, "enable camera and storage permission", Toast.LENGTH_SHORT);
+                        Toast.makeText(this, "Enable camera and storage permissions", Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(this, "Unexpected result from permission request", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case STORAGE_REQUEST:
-                if (grantResults.length > 0) {
+                if (grantResults.length >= 1) { // Check if grantResults array has at least one element
                     boolean storage_accept = grantResults[0] == PackageManager.PERMISSION_GRANTED;
 
                     if (storage_accept) {
                         pickFromGallery();
                     } else {
-                        Toast.makeText(this, "enable storage permission", Toast.LENGTH_SHORT);
+                        Toast.makeText(this, "Enable storage permission", Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(this, "Unexpected result from permission request", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case FINE_PERMISSION_CODE:
+                if (grantResults.length >= 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastLocation();
+                } else {
+                    // Location permission denied
+                    Toast.makeText(this, "Location permission is required!", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
